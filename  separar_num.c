9f0cc93c9aec0354eb53f8073eb253f93adf270f
jhonny arana <jhonnyjosearana@gmail.com>
2015-05-18 15:29:54 -0430

    subiendo todo

---
+++  separar_num.c
@@ -0,0 +1,16 @@
+/* programa que separa un entero de 2 digitos */
+#include <stdio.h>
+int main(){
+  int num;
+  int digito_der;
+  int digito_izq;
+   printf("introduszca el numero de 2 cifras");
+   scanf("%d", &num);
+      digito_izq = num/10;
+      digito_der = num%10;
+    printf("el numero a separar es%d\n", num);
+    printf("el numero izquierdo es %d\n", digito_izq);
+    printf("el numero iderecho es %d\n", digito_der);
+  
+  return 0;
+}
--- holamundo.c
+++ holamundo.c
@@ -1,8 +1,18 @@
+/* programa.c -- 
+ * author: Francisco Granados
+ * date: MM DD YYYY
+*/
 
 #include <stdio.h>
 
-int main()
+int main(int argc, char const *argv[])
 {
-	printf("hola mundo");
-	
+	char twitter[140];
+	int tweets;
+
+	printf("Twitter username: @");
+	scanf("%s %d", twitter, &tweets);
+
+	printf("https://twitter.com/%s (%d tweets)\n", twitter, tweets);
+	return 0;
 }
