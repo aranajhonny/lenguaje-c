65b9072a860e38b2a699e4be25d52b0cda8c7bc5
jhonny <jhonnyjosearana@gmail.com>
2015-05-28 06:25:17 -0430

    ultimos cambios

--- fibonacci.c
+++ fibonacci.c
@@ -1,18 +1,18 @@
 #include<stdio.h>
 int main()
 {	int suma=0,b=1,a=0, x;
-	printf ("Digitar el numero donde termine la serie de Fibonacci: ");
+	printf ("introduzca un numero: ");
 	scanf("%d",&x);
 		while(suma<=x){
 			suma=b+a;
 			a = b;
 			b = suma;
 		if (suma==x)
-			printf("\n pertenece a la serie");
+			printf("\n pertenece a la serie fibonacci");
 		
 	}
 		if (suma!=x)
-			printf("%i\n",suma);
+			printf("%i\n el siguiente numero de fibonacci es:",suma);
 		return 0;
 
 }
