abfca11019354f1ad1812e2e988881f1ead30147
jhonny arana <jhonny@localhost.localdomain>
2015-06-03 22:23:54 -0430

    :octocat: agregando ejercicios de ajenos:penguin:

---
+++ Hello_World/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/*
+* This program takes a simple message and prints it out to the screen
+*/
+
+
+int main()
+{
+    printf("One small step for programmers\n");
+    printf("One giant leap for mankind!\n");
+    return 0;
+    // the \n character signifies a 'newline'.
+    // the return statement can return any value you specify, but 0 seems to be a best practice.
+    // A return statement is not even required to run the main () function, but including a return statement seems to be a best practice as well
+}
